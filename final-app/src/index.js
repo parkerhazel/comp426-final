3f76f3671cb3b86e0fec3746c64a78fd4372b33e
Parker Hazel <phazel@gorelickbrothers.com>
2021-05-06 15:13:07 -0400

    Revert "Firebase stuff"
    This reverts commit 02b5f332f42df54c7f276c3b141cc8ad399d2ac8.

--- final-app/src/index.js
+++ final-app/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as firebase from 'firebase';
-
-// firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
